cd1eb30707ed32013d91cedacf5b9312776da23b
user <user@192.168.1.19>
2025-06-19 18:01:46 +0900

    カリキュラム22

---
+++ curriculum22/src/packagA/PackA.java
@@ -0,0 +1,12 @@
+package packagA;
+
+import packageB.PackB;
+
+public class PackA {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		PackB country = new PackB("日本", "寿司"); //代入
+		country.swoInfo(); //呼び出し
+	}
+}
---
+++ curriculum22/src/packageB/PackB.java
@@ -0,0 +1,32 @@
+package packageB;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class PackB {
+	String country;
+	String food;
+
+	public PackB(String country, String food) {
+		this.country = country;
+		this.food = food;
+	}
+
+	public void swoInfo() {
+		System.out.println("こんにちは！ここは" + country + "です！");
+		//こんにちは！ここは日本です！
+		System.out.println("この" + food + "はうまい");
+		//この寿司はうまい
+		System.out.println("" + food + "は和食です");
+		//寿司は和食です
+		LocalDateTime now = LocalDateTime.now(); //日時を取得
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+		//出力形式の指定
+		String formattedDate = now.format(formatter);
+		System.out.println("今の現在日時は" + formattedDate + "です");
+		//今の現在日時は2023/03/09 10:23:39です
+	}
+}
+
+//thisを使って作成してください。
+//日時は今日の日付を取得してください。
